270ef31e675462ce641ed81cdfb8c941161c450e
nmschuler <nm_schuler@yahoo.com>
2016-11-22 15:41:37 -0500

    edited file to allow new section

--- js/layout.js
+++ js/layout.js
@@ -11,6 +11,9 @@ function loadNavbar(){
             '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
                '<li class="#"><a href="subjects/classes.html">Classes</a></li>' +
             '</ul>' +
+            '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
+               '<li class="#"><a href="sections/sections.html">Sections</a></li>' +
+            '</ul>' +
             '<ul class="nav navbar-nav navbar-right" style="padding-right:50px">' +
                '<li class="dropdown">' +
                   '<a class="dropdown-toggle" data-toggle="dropdown">Nick Schuler' +
@@ -39,6 +42,9 @@ function loadNavbar(){
                '</ul>' +
                '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
                   '<li class="#"><a href="classes.html">Classes</a></li>' +
+               '</ul>' +
+               '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
+                  '<li class="#"><a href="../sections/sections.html">Sections</a></li>' +
                '</ul>' +
                '<ul class="nav navbar-nav navbar-right" style="padding-right:50px">' +
                   '<li><a href="#" onclick="return false">Nick Schuler</a></li>' +
@@ -75,3 +81,45 @@ function loadSidebarClasses(){
         '</ul>' +
       '</div>';
    }
+
+
+   function loadNavbarSections(){
+      document.getElementById("navMenu").innerHTML =
+         '<div class="navbar navbar-inverse navbar-static-top">' +
+            '<div class="collapse navbar-collapse navHeaderCollapse">' +
+               '<ul class="nav navbar-nav navbar-left" style="padding-left:50px">' +
+                  '<li class="#"><a href="../index.html">Home</a></li>' +
+               '</ul>' +
+               '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
+                  '<li class="#"><a href="../about.html">About Me</a></li>' +
+               '</ul>' +
+               '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
+                  '<li class="#"><a href="../subjects/classes.html">Classes</a></li>' +
+               '</ul>' +
+               '<ul class="nav navbar-nav navbar-left" style="padding-left:15px">' +
+                  '<li class="#"><a href="sections.html">Sections</a></li>' +
+               '</ul>' +
+               '<ul class="nav navbar-nav navbar-right" style="padding-right:50px">' +
+                  '<li><a href="#" onclick="return false">Nick Schuler</a></li>' +
+               '</ul>' +
+            '</div>' +
+         '</div>';
+      }
+
+
+   function loadSidebarSections(){
+      document.getElementById("sidebarSections").innerHTML =
+         '<div class="navbar navbar-static-left">' +
+           '<ul class="nav navbar-stacked" style="padding-top:30px">' +
+            '<h4>Sections</h4>' +
+            '<li><a href="sections.html">Overview</a></li>' +
+            '<li><a href="database.html">Database</a></li>' +
+            '<li><a href="#">blah2</a></li>' +
+            '<li><a href="#">blah3</a></li>' +
+            '<br />' +
+            '<br />' +
+            '<br />' +
+            '<br />' +
+           '</ul>' +
+         '</div>';
+      }
